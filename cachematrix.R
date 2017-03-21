6d778f87cc01cd8e7ec3535291b28b87cf9df53a
Lukas2010 <lukas.koenen@gmail.com>
2017-03-21 12:40:52 +0100

    coursera assignment week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Hello, i created two functions in which the inverse of a matrix is calculated
+## when there is an existing inverse calculation of a matrix this one is retrieved
+## thanks for reviewing it !
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric(), a = numeric(), b = numeric()) {
+  s <- NULL
+  set <- function(y, a = 1, b = 1) {
+    x <<- y
+    row_a <<- a
+    col_b <<- b
+    s <<- NULL
+  }
+  get <- function() matrix(x, a, b)
+  setinverse <- function(solve) s <<- solve
+  getinverse <- function() s
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
+## The first functions main goal is to cache a matrix which is provided to this
+## function. 
+## I did not use x = matrix() and wrote every argument, while putting the matrix
+## function at the get function
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheinverse <- function(x, ...) {
+  s <- x$getinverse()
+  if(!is.null(s)) {
+    message("getting cached matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data,...)
+  x$setinverse(s)
+  s
 }
+
+## This function will calculate the inverse of a matrix upplied to it while
+## checking beforehand whether the cache is filled yet with a if statement.
